3fc0651e2634ca85fa787422ce94fb0995696a89
NecatiCoskuncopur <necaticoskuncopur@gmail.com>
2026-02-28 00:28:22 +0300

    feat(PAGE=>ACCOUNT): add AccountPage & layout

---
+++ src/components/Dashboard/Account/index.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useState } from 'react';
+
+import { Box, Paper, Tab, Tabs } from '@mui/material';
+
+const ProfileSections = () => {
+  const [value, setValue] = useState<number>(0);
+
+  const handleChange = (_: React.SyntheticEvent, newValue: number): void => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper sx={{ width: '100%', p: 3 }}>
+      <Tabs value={value} onChange={handleChange} textColor="primary" indicatorColor="primary">
+        <Tab label="Düzenle" />
+        <Tab label="Güvenlik" />
+      </Tabs>
+
+      <Box sx={{ mt: 3 }}>
+        {/* {value === 0 && <EditUserForm />}
+        {value === 1 && <ChangePasswordForm />} */}
+      </Box>
+    </Paper>
+  );
+};
+
+export default ProfileSections;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export { default as ForgotPasswordForm } from './Auth/ForgotPasswordForm';
 export { default as ResetPasswordForm } from './Auth/ResetPasswordForm';
 export { default as SignInForm } from './Auth/SignInForm';
 export { default as SignUpForm } from './Auth/SignUpForm';
+export { default as Account } from './Dashboard/Account';
 export { default as DashboardShell } from './Dashboard/Layout/DashboardShell';
 export { default as UserBalanceTable } from './Dashboard/UserBalanceTable';
 export { default as UserPricingListTable } from './Dashboard/UserPricingListTable/UserPricingListTable';
