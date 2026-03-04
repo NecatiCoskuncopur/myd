909f4f8473260b95f3a7ffbbd682f6da53fae2e9
NecatiCoskuncopur <necaticoskuncopur@gmail.com>
2026-03-04 18:23:26 +0300

    feat(UTIL): implement balance transaction handler

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,4 +1,9 @@
 const messages = {
+  BALANCE: {
+    ERROR: 'Bakiye işlenirken hata oluştu!',
+    INVALID: 'Bakiye işlem tipi geçersiz.',
+    NOT_FOUND: 'Bakiye bulunamadı',
+  },
   GENERAL: {
     UNEXPECTED_ERROR: 'Beklenmeyen bir hata oluştu',
     UNAUTHORIZED: 'Yetkisiz işlem',
