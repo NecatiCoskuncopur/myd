9cad828a522511b037398cc490610f55972a1dbb
NecatiCoskuncopur <necaticoskuncopur@gmail.com>
2026-03-04 07:05:06 +0300

    feat(MODEL): add User model

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,4 @@ export { default as CarrierAccount } from './CarrierAccount.model';
 export { default as Consignee } from './Consignee.model';
 export { default as PricingList } from './PricingList.model';
 export { default as Shipping } from './Shipping.model';
+export { default as User } from './User.model';
