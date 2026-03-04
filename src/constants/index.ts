c4e80e2922f5fde41ddcec7399a3661ff6ee0166
NecatiCoskuncopur <necaticoskuncopur@gmail.com>
2026-03-04 20:33:23 +0300

    feat: add  email templates as constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,5 @@
+export { default as company } from './company';
 export { default as countries } from './countries';
+export { default as forgotPasswordMail } from './mailTemplates/forgotPassword.mail';
+export { default as welcomeMail } from './mailTemplates/welcome.mail';
 export { default as messages } from './messages';
