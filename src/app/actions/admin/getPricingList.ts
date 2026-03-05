54e9f0cd5f7d25dcadeab241bd0cd0f20575b759
NecatiCoskuncopur <necaticoskuncopur@gmail.com>
2026-03-05 05:31:06 +0300

    feat(ACTION=>ADMIN):  add getPricingList action

---
+++ src/app/actions/admin/getPricingList.ts
@@ -0,0 +1,56 @@
+'use server';
+
+import { Types } from 'mongoose';
+
+import { messages } from '@/constants';
+import connectMongoDB from '@/lib/db';
+import requireRoles from '@/lib/requireRoles';
+import { PricingList } from '@/models';
+
+const { GENERAL, PRICINGLIST } = messages;
+
+const getPricingList = async (listId: string): Promise<IActionResponse<IPricingList>> => {
+  try {
+    const authError = await requireRoles(['OPERATOR', 'ADMIN']);
+    if (authError) return authError;
+
+    if (!Types.ObjectId.isValid(listId)) {
+      return {
+        status: 'ERROR',
+        message: PRICINGLIST.NOT_FOUND,
+      };
+    }
+    await connectMongoDB();
+
+    const pricingListDoc = await PricingList.findById(listId).lean<IPricingList>();
+
+    if (!pricingListDoc) {
+      return {
+        status: 'ERROR',
+        message: PRICINGLIST.NOT_FOUND,
+      };
+    }
+
+    const pricingList: IPricingList = {
+      _id: pricingListDoc._id.toString(),
+      name: pricingListDoc.name,
+      zone: pricingListDoc.zone.map((z: IZone) => ({
+        number: z.number,
+        prices: z.prices.map((p: IPrice) => ({ weight: p.weight, price: p.price })),
+        than: z.than,
+      })),
+    };
+
+    return {
+      status: 'OK',
+      data: pricingList,
+    };
+  } catch {
+    return {
+      status: 'ERROR',
+      message: GENERAL.UNEXPECTED_ERROR,
+    };
+  }
+};
+
+export default getPricingList;
