88a5871d443177a81ff378f5630a4b2acf0e75e5
NecatiCoskuncopur <necaticoskuncopur@gmail.com>
2026-03-06 20:41:17 +0300

    feat(AUTH): add Hcaptcha component

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,4 +2,17 @@ import mongoose from 'mongoose';
 
 declare global {
   var mongoose: { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null };
+  interface Window {
+    hcaptcha?: {
+      render: (
+        container: HTMLElement,
+        options: {
+          sitekey: string;
+          callback: (token: string) => void;
+          'expired-callback'?: () => void;
+        },
+      ) => number;
+      remove: (widgetId: number) => void;
+    };
+  }
 }
