4e19e0d0c0b1c89c7fefd869d5f9ccfd5ad9a368
NecatiCoskuncopur <necaticoskuncopur@gmail.com>
2026-04-02 01:34:56 +0300

    feat(UTIL): add validateRecaptcha util

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 export { default as addressMessages } from './messages/addressMessages';
 export { default as authMessages } from './messages/authMessages';
+export { default as captchaMessages } from './messages/captchaMessages';
 export { default as generalMessages } from './messages/generalMessages';
 export { default as userMessages } from './messages/userMessages';
