b6d4f257ccf2e9bf21628fb2819191e26d059442
NecatiCoskuncopur <necaticoskuncopur@gmail.com>
2026-05-05 12:12:26 +0300

    feat: PricingList modeli eklendi

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export { default as User } from './User.model';
 export { default as AddressSchema } from './AddressSchema';
 export { default as Balance } from './Balance.model';
+export { default as PricingList } from './PricingList.model';
