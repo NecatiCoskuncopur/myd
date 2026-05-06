5ccbc6d096cc99a6637a3f6644855e59e8f08ccb
NecatiCoskuncopur <necaticoskuncopur@gmail.com>
2026-05-06 13:21:44 +0300

    feat: CarrierAccount modeli eklendi

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export { default as User } from './User.model';
 export { default as AddressSchema } from './AddressSchema';
 export { default as Balance } from './Balance.model';
 export { default as PricingList } from './PricingList.model';
+export { default as CarrierAccount } from './CarrierAccount.model';
