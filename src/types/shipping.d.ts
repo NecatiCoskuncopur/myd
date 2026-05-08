52446eaa7e17f334821a6582a7e72b128d349e8d
NecatiCoskuncopur <necaticoskuncopur@gmail.com>
2026-05-08 09:58:43 +0300

    feat: getShipping action eklendi

--- src/types/shipping.d.ts
+++ src/types/shipping.d.ts
@@ -3,4 +3,95 @@ declare namespace ShippingTypes {
     weight: number;
     countryCode: string;
   }
+
+  interface IBaseAddress {
+    line1: string;
+    line2?: string;
+    city: string;
+    postalCode: string;
+  }
+
+  interface ISenderAddress extends IBaseAddress {
+    district: string;
+  }
+
+  interface IConsigneeAddress extends UserType.IBaseAddress {
+    country: string;
+    state?: string;
+  }
+
+  interface ISender {
+    name: string;
+    company?: string;
+    phone: string;
+    email: string;
+    address: ISenderAddress;
+  }
+
+  interface IConsignee {
+    name: string;
+    company: string;
+    phone?: string;
+    email?: string;
+    taxId?: string;
+    address: IConsigneeAddress;
+  }
+
+  interface IShippingDetail {
+    payor?: {
+      shipping?: 'SENDER' | 'CONSIGNEE';
+      customs?: 'SENDER' | 'CONSIGNEE';
+    };
+    iossNumber?: string;
+    purpose?: 'GIFT' | 'PERSONAL' | 'SAMPLE' | 'REPAIR_OR_RETURN' | 'COMMERCIAL';
+  }
+
+  interface IProduct {
+    name: string;
+    unitPrice: number;
+    piece: number;
+    gtip?: string;
+  }
+
+  interface IShippingContent {
+    currency?: 'USD' | 'EUR' | 'GBP';
+    description?: string;
+    freight?: number;
+    products?: IProduct[];
+  }
+
+  interface IPackage {
+    weight?: number;
+    numberOfPackage?: number;
+    width?: number;
+    height?: number;
+    length?: number;
+    volumetricWeight?: number;
+  }
+
+  interface IShipping {
+    _id: string;
+    userId: string;
+    consigneeId: string;
+    sender: ISender;
+    consignee: IConsignee;
+    detail: IShippingDetail;
+    content: IShippingContent;
+    package: IPackage;
+    status?: 'CREATED' | 'LABELED' | 'CANCELED';
+    carrier?: {
+      name?: 'FEDEX' | 'TNT' | 'UPS';
+      account?: string;
+      trackingNumber?: string;
+      amount?: number;
+    };
+    labelLink?: string;
+    activities?: {
+      userId?: string;
+      type?: 'EDIT' | 'LABELING';
+      data?: string;
+    }[];
+    createdAt: string;
+    updatedAt: string;
+  }
 }
